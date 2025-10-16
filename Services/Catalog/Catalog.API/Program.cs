64016bcadfc1e13d3bc9a48932c831aab51ca533
Gabriel Feitosa <63379991+gabrielfeitosac@users.noreply.github.com>
2025-10-15 23:26:04 -0300

    feat: initial implementation of createProduct

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,23 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);    
+}).UseLightweightSessions(); 
 
 var app = builder.Build();
 
 //Configure Http request pipeline
+app.MapCarter();
 
 app.Run();
